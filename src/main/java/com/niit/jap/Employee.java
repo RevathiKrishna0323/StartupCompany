d0f5a43e2de3addb90f245ddf325a559d00130da
Revathi G <revathikrishna0323@gmail.com>
2022-10-10 11:40:12 +0530

    created attributes of the Employee class

--- src/main/java/com/niit/jap/Employee.java
+++ src/main/java/com/niit/jap/Employee.java
@@ -6,5 +6,12 @@
 
 package com.niit.jap;
 
+import java.time.LocalDate;
+
 public class Employee {
+    String name;
+    int age;
+    LocalDate dateOfBirth;
+    String adress;
+    double salary;
 }
